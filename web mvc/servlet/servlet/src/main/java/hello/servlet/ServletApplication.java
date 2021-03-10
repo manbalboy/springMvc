a9baef33f07260d120449e3cba5051da4344a117
manbalboy <manbalboy@hanmail.net>
2021-03-11 00:11:48 +0900

    servlet 테스트

--- web mvc/servlet/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ web mvc/servlet/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 public class ServletApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
